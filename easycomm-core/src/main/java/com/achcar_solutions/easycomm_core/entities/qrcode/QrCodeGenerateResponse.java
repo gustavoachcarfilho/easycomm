17c0a4debb653d8440e255e5adcb8a4ba1dd5924
unknown <gustavoachcarfilho@gmail.com>
2025-07-15 14:35:57 -0300

    Added QrCodeGenrateResponse documentation

--- easycomm-core/src/main/java/com/achcar_solutions/easycomm_core/entities/qrcode/QrCodeGenerateResponse.java
+++ easycomm-core/src/main/java/com/achcar_solutions/easycomm_core/entities/qrcode/QrCodeGenerateResponse.java
@@ -1,4 +1,8 @@
 package com.achcar_solutions.easycomm_core.entities.qrcode;
 
-public record QrCodeGenerateResponse(String url) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record QrCodeGenerateResponse(
+        @Schema(description = "A URL pública da imagem do QR Code gerado e salvo na S3.")
+        String url) {
 }
